976880e23ee54da06ac16a0151af1a6b501a8329
Grace <gxu660@aucklanduni.ac.nz>
2024-08-03 15:01:44 +1200

    Made redirect links on discover page to google.com

--- src/pages/discover.jsx
+++ src/pages/discover.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import './discover.css'; // Import a CSS file for styling (optional)
+import '../styles/discover.css';
+import cheeseburger from '../resources/images/cheesburger.jpg';
 
 const blocks = [
-  { imgSrc: 'image1.jpg', link: 'https://example.com/page1' },
-  { imgSrc: 'image2.jpg', link: 'https://example.com/page2' },
-  { imgSrc: 'image3.jpg', link: 'https://example.com/page3' },
+  { imgSrc: cheeseburger, link: 'https://google.com' },
+  { imgSrc: cheeseburger, link: 'https://google.com' },
+  { imgSrc: cheeseburger, link: 'https://google.com' },
   // Add more blocks as needed
 ];
 
 function DiscoverPage() {
   return (
     <div className="simple-page">
-      <h1>Hello, Welcome to My React Page!</h1>
+      <h1>Discover</h1>
       <p>This is a simple page displaying some text.</p>
       <div className="blocks-container">
         {blocks.map((block, index) => (
