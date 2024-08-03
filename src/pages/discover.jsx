c81c163feacdd2417b6b46681e7a2d4f107b128c
kimkimz <kimberleymzhu@gmail.com>
2024-08-04 11:48:39 +1200

    trying to fix styling issues of the discover page

--- src/pages/discover.jsx
+++ src/pages/discover.jsx
@@ -1,47 +1,89 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/discover.css';
 import cheeseburger from '../resources/images/cheesburger.jpg';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './home'
-import Discover from './discover'
-import { Link } from 'react-router-dom'
-
-
 function DiscoverPage() {
   const [selectedFilter, setSelectedFilter] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showFavorites, setShowFavorites] = useState(false);
 
   const blocks1 = [
-    { imgSrc: cheeseburger, link: '/restaurants', type: 'local', name: 'Galactic Grill & Tacos', dietary: ['vegan', 'gluten-free'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Nebula Noodles', dietary: ['vegetarian'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Stellar Sushi', dietary: ['gluten-free'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'McGalaxy', dietary: ['gluten-free', 'vegetarian'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'KFCosmic', dietary: ['vegan'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'FiveAliens', dietary: ['gluten-free'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Pulsar Pizza', dietary: ['vegetarian'] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Supernova Steak', dietary: [] },
-    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Comet Café', dietary: ['vegan', 'vegetarian'] },
-    // Add more blocks as needed
+    { imgSrc: cheeseburger, link: '/restaurants', type: 'local', name: 'Galactic Grill & Tacos', dietary: ['vegan', 'gluten-free'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Nebula Noodles', dietary: ['vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Stellar Sushi', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'McGalaxy', dietary: ['gluten-free', 'vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'KFCosmic', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'FiveAliens', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Pulsar Pizza', dietary: ['vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Supernova Steak', dietary: [], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Comet Café', dietary: ['vegan', 'vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Cosmic Curry', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Meteorite Munchies', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Planetary Pizza', dietary: ['vegetarian'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Starbucks', dietary: ['gluten-free', 'vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Galactic Gourmet', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Asteroid Appetizers', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Celestial Cuisine', dietary: ['vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Quantum Quiche', dietary: [], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Nebula Nibbles', dietary: ['vegan', 'vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Solar Sizzlers', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Intergalactic Ice Cream', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Comet Confections', dietary: ['vegetarian'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Alien Abode', dietary: ['gluten-free', 'vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Galactic Grains', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'earth', name: 'Planetary Patisserie', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Universal Udon', dietary: ['vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Cosmic Café', dietary: [], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'interstellar', name: 'Nebula Nosh', dietary: ['vegan', 'vegetarian'], favorite: false }
   ];
-
 
   const filteredBlocks = selectedFilter
     ? blocks1.filter(block => block.dietary.includes(selectedFilter))
     : blocks1;
 
+  const searchedBlocks = searchQuery
+    ? filteredBlocks.filter(block => block.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    : filteredBlocks;
+
+  const finalBlocks = showFavorites
+    ? searchedBlocks.filter(block => block.favorite)
+    : searchedBlocks;
+
   return (
     <div className="simple-page">
       <h1>Discover</h1>
-      <div className="filters">
-        <button onClick={() => setSelectedFilter('vegan')}>Vegan</button>
-        <button onClick={() => setSelectedFilter('vegetarian')}>Vegetarian</button>
-        <button onClick={() => setSelectedFilter('gluten-free')}>Gluten-Free</button>
-        <button onClick={() => setSelectedFilter('')}>All</button>
-      </div>
 
+      <div className="discover-nav">
+        <div className="search-bar">
+          <form className="search-form">
+            <input type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)} className="search-input" />
+            <button type="submit" className="search-button">Search</button>
+          </form>
+        </div>
+        <div className="filters">
+          <button onClick={() => setSelectedFilter('vegan')}>Vegan</button>
+          <button onClick={() => setSelectedFilter('vegetarian')}>Vegetarian</button>
+          <button onClick={() => setSelectedFilter('gluten-free')}>Gluten-Free</button>
+          <button onClick={() => setSelectedFilter('')}>All</button>
+        </div>
+        <div className="favorites-toggle">
+          <button onClick={() => setShowFavorites(!showFavorites)}>
+            {showFavorites ? 'Show All' : 'Show Favorites'}
+          </button>
+        </div>
+      </div>
       <h2>Local</h2>
-      <div className="blocks-container scrollable">
-        {filteredBlocks.filter(block => block.type === 'local').map((block, index) => (
+      {finalBlocks.filter(block => block.type === 'local').length > 4 && (
+        <Link to="/local">
+          <button>Show More</button>
+        </Link>
+      )}
+      <div className="blocks-container">
+        {finalBlocks.filter(block => block.type === 'local').slice(0, 4).map((block, index) => (
           <Link to={block.link} key={index} className="block">
             <div className="imgWrapper">
               <img src={block.imgSrc} alt={`Block ${index + 1}`} />
@@ -50,9 +92,15 @@ function DiscoverPage() {
           </Link>
         ))}
       </div>
+
       <h2>Earth</h2>
-      <div className="blocks-container scrollable">
-        {filteredBlocks.filter(block => block.type === 'earth').map((block, index) => (
+      {finalBlocks.filter(block => block.type === 'earth').length > 4 && (
+        <Link to="/earth">
+          <button>Show More</button>
+        </Link>
+      )}
+      <div className="blocks-container">
+        {finalBlocks.filter(block => block.type === 'earth').slice(0, 4).map((block, index) => (
           <a href={block.link} key={index} className="block">
             <div className="imgWrapper">
               <img src={block.imgSrc} alt={`Block ${index + 1}`} />
@@ -61,18 +109,27 @@ function DiscoverPage() {
           </a>
         ))}
       </div>
+
       <h2>Interstellar</h2>
-      <div className="blocks-container scrollable">
-        {filteredBlocks.filter(block => block.type === 'interstellar').map((block, index) => (
+      {finalBlocks.filter(block => block.type === 'interstellar').length > 4 && (
+        <Link to="/interstellar">
+          <button>Show More</button>
+        </Link>
+      )}
+      <div className="blocks-container">
+        {finalBlocks.filter(block => block.type === 'interstellar').slice(0, 4).map((block, index) => (
           <a href={block.link} key={index} className="block">
-            <div className="imgWrapper">
-              <img src={block.imgSrc} alt={`Block ${index + 1}`} />
+            <div className="block-card">
+              <div className="imgWrapper">
+                <img src={block.imgSrc} alt={`Block ${index + 1}`} />
+              </div>
+              <p style={{ textAlign: 'center' }}>{block.name}</p>
             </div>
-            <p style={{ textAlign: 'center' }}>{block.name}</p>
           </a>
         ))}
       </div>
     </div>
   );
 }
+
 export default DiscoverPage;
---
+++ src/pages/local.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import cheeseburger from '../resources/images/cheesburger.jpg';
+
+function LocalPage() {
+  const blocks = [
+    { imgSrc: cheeseburger, link: '/restaurants', type: 'local', name: 'Galactic Grill & Tacos', dietary: ['vegan', 'gluten-free'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Nebula Noodles', dietary: ['vegetarian'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Stellar Sushi', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Cosmic Curry', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Meteorite Munchies', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Planetary Pizza', dietary: ['vegetarian'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Solar Sizzlers', dietary: ['vegan'], favorite: true },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Intergalactic Ice Cream', dietary: ['gluten-free'], favorite: false },
+    { imgSrc: cheeseburger, link: 'https://google.com', type: 'local', name: 'Comet Confections', dietary: ['vegetarian'], favorite: true }
+  ];
+
+  return (
+    <div className="simple-page">
+      <h1>Local Cuisine</h1>
+      <div className="blocks-container">
+        {blocks.map((block, index) => (
+          <Link to={block.link} key={index} className="block">
+            <div className="imgWrapper">
+              <img src={block.imgSrc} alt={`Block ${index + 1}`} />
+            </div>
+            <p style={{ textAlign: 'center' }}>{block.name}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LocalPage;
