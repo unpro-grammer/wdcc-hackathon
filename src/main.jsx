81a51388a2dd47a3371f88b7003086f097161fbb
Grace <gxu660@aucklanduni.ac.nz>
2024-08-04 01:47:46 +1200

    Global new font

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/home.css"
 import './index.css'
+import { useEffect } from 'react';
+
+const Main = () => {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap';
+    document.head.appendChild(link);
+
+    // Cleanup function to remove the link when the component unmounts
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
+  return <App />;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
