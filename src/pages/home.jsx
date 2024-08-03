d29fd4b54f1196bfe8595a94ea352ae7ad9a9583
kimkimz <kimberleymzhu@gmail.com>
2024-08-03 17:21:46 +1200

    making basic homepage, not working yet

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,19 +5,29 @@ import '../styles/home.css'; // Go up one directory level from src/pages
 const Home = () => {
   return (
     <html lang="en">
-      <body className="main-home">
-        <div className="home-container">
-          <image src={backgroundImg} alt="background image" className="home-background" />
-          <div className="home-contents">
-            <h1>Welcome to Space Bites</h1>
-            <h2>Out of this world flavour!</h2>
-            <p>Space Bites is the best place to get your snacks! We have a wide variety of snacks to choose from, all with a unique space theme. Our snacks are out of this world!</p>
+      <body>
+        <div className="main-home">
+          <div className="home-container">
+            <image src={backgroundImg} alt="background image" className="home-background" />
+            <div className="home-contents">
+              <h1>Welcome to Space Bites</h1>
+              <h2>Out of this world flavour!</h2>
+              <p>Space Bites is the best place to get your snacks! We have a wide variety of snacks to choose from, all with a unique space theme. Our snacks are out of this world!</p>
+            </div>
+          </div>
+          <div className="nav-cards">
+            <div className="nav-card">
+              <h3>Local Food</h3>
+            </div>
+            <div className="nav-card">
+              <h3>Earth Food</h3>
+            </div>
+            <div className="nav-card">
+              <h3>Interstellar Food</h3>
+            </div>
           </div>
         </div>
       </body>
-      <footer className="home-footer">
-        <p>footer information there is stuff</p>
-      </footer>
     </html>
   );
 };
