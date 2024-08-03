f88250c6b8f58aeb2f3bea48c0bcd0ed13e5dce8
Grace <gxu660@aucklanduni.ac.nz>
2024-08-03 18:32:21 +1200

    Make list of foods that the restaurant has to offer

--- src/pages/resteraunts.jsx
+++ src/pages/resteraunts.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/resteraunts.css';
-import cheeseburger from '../resources/images/cheesburger.jpg';
+import cheeseburger from '../resources/images/cheesburger.jpg'; // Corrected typo
 
 const blocks1 = [
     { imgSrc: cheeseburger, link: 'https://google.com' },
@@ -9,6 +9,10 @@ const blocks1 = [
 
 const foodItems = [
     { imgSrc: cheeseburger, description: 'Delicious Cheeseburger', price: '$5.99' },
+    { imgSrc: cheeseburger, description: 'Yummy Fries', price: '$2.99' },
+    { imgSrc: cheeseburger, description: 'Tasty Milkshake', price: '$3.99' },
+    { imgSrc: cheeseburger, description: 'Scrumptious Salad', price: '$4.99' },
+    { imgSrc: cheeseburger, description: 'Delightful Ice Cream', price: '$3.99' },
     // Add more food items as needed
 ];
 
@@ -35,8 +39,10 @@ function RestaurantPage() {
                     {items.map((item, index) => (
                         <li key={index} className="food-item">
                             <img src={item.imgSrc} alt={`Food Item ${index + 1}`} />
-                            <p>{item.description}</p>
-                            <p>{item.price}</p>
+                            <div className="item-details">
+                                <p>{item.description}</p>
+                                <p>{item.price}</p>
+                            </div>
                             <button>Add to Cart</button>
                         </li>
                     ))}
