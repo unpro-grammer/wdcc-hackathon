fc436a626b764d8121775f36ed07925977f26bbb
JackDMatthews <135397534+JackDMatthews@users.noreply.github.com>
2024-08-03 16:04:03 +1200

    the navbar links work

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import DiscoverPage from './pages/discover.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DiscoverPage />
+    <App />
   </React.StrictMode>,
 )
